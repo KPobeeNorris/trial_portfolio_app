0f55fbdc5aef1f451754372223689b7183263092
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-06-11 11:57:37 +0100

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'javascript-items', to: 'portfolios#javascript'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
