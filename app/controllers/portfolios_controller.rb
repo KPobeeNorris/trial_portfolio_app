bf1ee4c557b2abd66cafb98df4ef35ef546e2ba7
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-06-12 14:44:08 +0100

    Refactored portfolio to include private set_portfolio method to keep it DRY

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -27,11 +27,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item was updated' }
@@ -42,11 +40,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio item was deleted.' }
