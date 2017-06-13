df70849a58077495ca6f4d04b6d57770e225dfdf
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-06-13 19:58:14 +0100

    Implemented authorisation engine for portfolios and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
 
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+  access all: [:show, :index, :javascript], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   layout 'portfolio'
 
   def index
